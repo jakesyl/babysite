547724ae6ec922655cb18c7b2d6a57b85d2db033
jakesyl <jake@lexake.com>
2014-03-30 00:58:45 -0400

    we're going to test this code naow!

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php 
 //You are entering a world of messy code, a commentless void "the scary door" now for some ascii art at the bottom of this file:
 
-
+//$vote = $_POST['Vote'];//WE CAN USE THIS WHEN ALEX FINISHES HIS STUFF this is likely to be the source of all our problems
+$vote = "gender|1;race|2;eco|3";
 
 //database = localhost
 //login = backbac2_9babies
 //db table= backbac2_baby
 //alex1998 = pass for user
-$con = mysqli_connect("localhost", "bacbbac2_9babies","alex1998", "backbac2_baby");
 
-
-
-
-
+$con = mysqli_connect("localhost", "bacbbac2_9babies","alex1998", "backbac2_baby");// how we connect to mysql for server use
+//$con = mysqli_connect("74.220.195.132", "bacbbac2_9babies","alex1998", "backbac2_baby");//for other use
+if ($con->connect_error){
+echo("you idiot");
+}
+else{
+echo "haha";
+}
+$split = eplode(";",$ui);//explodes by ;
+$arraylength = count($split);//counts how many thingas are in an array for a for loop.. :D
+//$arraylength++;//may prove to be necesary, but until then, screw that... :D
+for ($i=0, $i<$arraylength, $i++){//the purpose of this for loop is to go through everything that got exploded and make it into it's own thing
+// insert a ton of if's in here for every possibility for launch, each as a function
+$operatee = $split[$i];
+$topic = explode("|", $operatee);//what we're exploding by | sructure is topic|vote(int)
+$option = $topic[2];//it's a 2 for 1!!    
+$topic = $topic[1];//got rid of the fugly array     
+//$unessec = mysqli_query($con, "SELECT topic FROM baby where topic = ". $topic);
+$option = "option".$option;
+mysqli_query($con, "UPDATE baby SET ".$option." = option + 1 WHERE topic = '".$topic"'");//increments a table
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+}
 /*
 
 
