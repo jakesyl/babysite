b3b21204dc0f3300e991dbbec1b2dc1ef7014ed2
aep000 <alexparson1@gmail.com>
2014-03-29 23:33:54 -0400

    First vote stuff

---
+++ Vote.php
@@ -0,0 +1,13 @@
+<?php
+$vote = $_POST["votes"];
+
+$con=mysqli_connect("sql4.freesqldatabase.com:3306","sql434134","bY2%tL9%","sql434134");
+if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+else{
+    $result = mysqli_query($con,"INSERT INTO Vote VALUES ");
+}
+//hfakshljkfa
+?>
